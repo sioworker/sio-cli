826c140e15f007573c4bd9baa93b225581ddfd86
sioworker <leon@winkids.pl>
2026-09-22 20:06:52 +0200

    Added completions for bash & zsh & powershell

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 BIN := $(HOME)/.local/bin
 FISH := $(HOME)/.config/fish/completions
+BASH := $(HOME)/.local/share/bash-completion/completions
+ZSH := $(HOME)/.local/share/zsh/site-functions
 
 install:
 	go build -o $(BIN)/sio ./cmd/sio
-	mkdir -p $(FISH)
+	mkdir -p $(FISH) $(BASH) $(ZSH)
 	cp completions/sio.fish $(FISH)/sio.fish
+	cp completions/sio.bash $(BASH)/sio
+	cp completions/_sio $(ZSH)/_sio
 
 .PHONY: install
---
+++ completions/sio.bash
@@ -0,0 +1,13 @@
+_sio() {
+	local cur=${COMP_WORDS[COMP_CWORD]} cmd=${COMP_WORDS[1]}
+	if ((COMP_CWORD == 1)); then
+		COMPREPLY=($(compgen -W 'add-host rm-host hosts main token ping upload' -- "$cur"))
+		return
+	fi
+	case $cmd in
+	add-host) ((COMP_CWORD == 2)) && COMPREPLY=($(compgen -W '--main' -- "$cur")) ;;
+	rm-host | main | token | ping) ((COMP_CWORD == 2)) && COMPREPLY=($(compgen -W "$(sio hosts 2>/dev/null | cut -c3- | cut -d' ' -f1)" -- "$cur")) ;;
+	upload | up) ((COMP_CWORD >= 3)) && COMPREPLY=($(compgen -f -- "$cur")) ;;
+	esac
+}
+complete -F _sio sio
